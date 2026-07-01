e9ff22d21a14defc608a07ea77109a0dd7fc390e
diyanraykovv <160146952+diyanraykovv@users.noreply.github.com>
2026-07-01 23:30:44 +0300

    v3
    Updated script to improve file naming and directory structure.

--- script-get-files.sh
+++ script-get-files.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-#below line deletes files from previous tries
-find . -type f -name 'L&D_1215_P_SOMETHING_SOMETHING_F_*' -delete && find . -type d -delete
+
+#delete files from previous execution of script
+#find . -type f -name 'L&D_1215_P_SOMETHING_SOMETHING_F_*' -delete && find . -type d -delete
 
 #dirs=$(date '+%Y%m')'0'$i/{'DELTA','FULL'}
-#put this in pattern for easyer reading
+
+#I use these so the file generation can be more readable
 dates=$(date +'%Y%m%d')
 datehrs=$(date +'%Y%m%d%H%M%S')
 for i in {1..9}
-do        
-        #this is a dir variable I did not put "$dirs"{DELTA,FULL} at the end because brace expansion does not work with variables 
-        #also put it in the loop to increment "i" variable so directories can be different
-        dirs=$(date +%Y%m)"0$i"/
-        mkdir -p "$dirs"{DELTA,FULL}
-        #creates every file
-        touch "$dirs"{DELTA,FULL}/L\&D_1215_P_SOMETHING_SOMETHING_F_"$dates"_"$datehrs".txt
+do
+        #I do not use /{DELTA,FULL} here because brace expansion is interpreted differently in variables
+        dirs=$(date +%Y%m)"0$i"/TABLE"$i"
+        mkdir -p "$dirs"/{DELTA,FULL}
+        #generates files
+        touch "$dirs"/{DELTA,FULL}/L\&D_1215_P_SOMETHING"$i"_SOMETHING"$i"_F_"$dates"_"$datehrs".txt
         tree
-     
-        sleep 1   #because I use seconds to create files
+        #needed for file generation since i use seconds to generate filenames
+        sleep 1
 done
+
